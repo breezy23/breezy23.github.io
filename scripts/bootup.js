f9904979ede105a0730d7b6e07323c2fa477bb8f
Brandon Zaragoza <brz0323@gmail.com>
2022-10-26 17:53:21 -0500

    Animated splash screen! :D

--- scripts/bootup.js
+++ scripts/bootup.js
@@ -17,6 +17,7 @@ const lines = [
 let cursor = true;
 const speed = 750;
 let script = true;
+let cont = false;
 
 const bzStr = [
 "88                             ,ad8888ba,     ad88888ba",
@@ -31,11 +32,15 @@ const bzStr = [
 
 bootLines()
 
-function bzOS(){
+function toHome(){
+	if(cont){
+		document.location.href = "pages/home.html";
+	}
+}
+async function bzOS(){
 	// TODO: Put text in new div
 	const splash = document.createElement("div");
 	const splashContainer = document.createElement("div");
-	const continueText= document.createElement("div");
 	const body = document.getElementById("body");
 	document.getElementById("textContainer").remove();
 
@@ -43,17 +48,37 @@ function bzOS(){
 
 	splash.classList.add("splash");
 	for(let i = 0; i < 8; i++){
-		let p = document.createTextNode("p");
-		let br = document.createElement("br");
+		const p = document.createTextNode("p");
+		const br = document.createElement("br");
 		p.textContent = bzStr[i];
 
 		splashContainer.appendChild(p);
 		splashContainer.appendChild(br);
 	}
 
+	splashContainer.style.top = window.innerHeight+"px";
 	splashContainer.style.textAlign = "left";
 	splash.appendChild(splashContainer);
 	body.appendChild(splash);
+
+	let height = window.innerHeight;
+	while(height > 200){
+		height -= 24;
+		splashContainer.style.top = height+"px";
+
+		await sleep(250);
+		cont = true;
+	}
+
+	await sleep(2000);
+
+	const p = document.createElement("div");
+	const br = document.createElement("br");
+	p.textContent = "---Press any key to continue---";
+	p.classList.add("splashContainer");
+
+	splashContainer.appendChild(br);
+	splashContainer.appendChild(p);
 }
 
 async function bootLines(){
@@ -88,53 +113,53 @@ async function bootLines(){
 		switch(i){
 			case 0:
 				// 'Disk check: OK.'
-				await sleep(1000);
+				await sleep(500);
 				break;
 			case 1:
 				// 'Memory check: OK.'
-				await sleep(1000);
+				await sleep(500);
 				break;
 			case 2:
 				// 'Hardware check: OK.'
-				await sleep(1000);
+				await sleep(500);
 				break;
 			case 3:
 				// 'Checking drivers...'
-				await sleep(1000);
+				await sleep(500);
 				break;
 			case 4:
 				// 'Driver version: V023'
 				break;
 			case 5:
 				// 'Driver name: MELON'
-				await sleep(4000);
+				await sleep(1000);
 				break;
 			case 6:
 				// 'Found!'
-				await sleep(1000);
+				await sleep(500);
 				break;
 			case 7:
 				// 'Driver version: V004'
 				break;
 			case 8:
 				// 'Driver name: FiNN'
-				await sleep(4000);
+				await sleep(1000);
 				break;
 			case 9:
 				// 'Found!'
-				await sleep(1000);
+				await sleep(500);
 				break;
 			case 10:
 				// 'All drivers found!'
-				await sleep(1000);
+				await sleep(500);
 				break;
 			case 11:
 				// 'Network check: OK.'
-				await sleep(500);
+				await sleep(250);
 				break;
 			case 12:
 				// 'Running bzOS...'
-				await sleep(7000);
+				await sleep(4000);
 				break;
 		}
 	}
