54302f76e2dcfd6e85085deecd93d948498ae409
Brandon Zaragoza <brz0323@gmail.com>
2022-11-25 20:14:11 -0600

    Added readme

--- scripts/readme.js
+++ scripts/readme.js
@@ -1,12 +1,19 @@
-let win = document.getElementById("window")
+let win = document.getElementById("window");
+const msg = [
+"Hello! This is a website that I decided to make because I was bored! :D",
+"I decided to make this website to explore the basics of web design, and keep me sane by doing something while I sit in my room instead of doing homework.",
+"To navigate around this website, use the up/down arrow keys to navigate, use right/left arrow keys to change which panel you are interacting with, and use enter to enter your selection.",
+"Alternatively, you could use the k/j and l/h keys respectively to navigate around"
+];
 
 win.innerText = "";
 
 function readme() {
-	let paragraph = document.createElement("p");
-	let text = document.createTextNode("Test1");
-	paragraph.appendChild(text);
-	text = document.createTextNode("Test2");
-	paragraph.appendChild(text);
-	win.appendChild(paragraph);
+	for(i = 0; i < msg.length; i++) {
+		let par = document.createElement("p");
+
+		par.textContent = msg[i];
+
+		win.appendChild(par);
+	}
 }
