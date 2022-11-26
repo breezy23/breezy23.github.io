bf6890414b93cf277d211476e120dd9c35251d5e
Brandon Zaragoza <brz0323@gmail.com>
2022-11-26 17:06:31 -0600

    Added projects page

---
+++ scripts/projectmanager.js
@@ -0,0 +1,46 @@
+const projects = [
+	"breezy23.github.io"
+];
+
+function createProjects() {
+	const div = document.createElement("div");
+	div.classList.add("dir");
+	div.id = "directory"
+
+	for(j = 0; j < 26; j++) {
+		const row = document.createElement("div");
+		const item = document.createElement("span");
+		const ident = document.createElement("span");
+
+		row.classList.add("row");
+		item.classList.add("item");
+		ident.classList.add("ident");
+
+		row.id = j;
+		if(j == 0){
+			item.textContent = "..";
+			ident.textContent = "-FOLDER-"
+			row.classList.add("selected")
+
+			row.append(item);
+			row.append(ident);
+			div.append(row);
+		}else{
+
+			if(j-1 < projects.length) {
+				item.textContent = projects[j-1];
+				ident.textContent = "--FILE->";
+
+				row.append(item);
+				row.append(ident);
+				div.append(row);
+			}else{
+				ident.textContent = "        ";
+				div.append(ident);
+			}
+		}
+	}
+
+	return div;
+}
+
