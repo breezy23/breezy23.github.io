97cb37993e91a1439c0b351266dfe04f669329bb
unknown <brz0323@gmail.com>
2023-07-29 23:38:44 -0500

    build: setup basis for sass

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -2,10 +2,13 @@ import * as React from 'react';
 import SelectionPanel from '../selection-panel/selection-panel';
 import ViewContainer from '../view-container/view-container';
 import Path from '../path/path';
+import './home.scss'
 
 function Home() {
     return (
-        <div>
+        <div
+            className={'home'}
+        >
             <SelectionPanel />
             <ViewContainer />
             <Path />
