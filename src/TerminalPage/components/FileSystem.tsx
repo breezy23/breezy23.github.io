2697c77a4ff48dab31e03e17d001945295bd2b05
unknown <brz0323@gmail.com>
2025-07-20 17:56:44 -0500

    Populate sidebar with temp files

--- src/TerminalPage/components/FileSystem.tsx
+++ src/TerminalPage/components/FileSystem.tsx
@@ -1,14 +1,57 @@
-import { FileSelector } from './FileSelector.tsx'
 import styles from '../TerminalPage.module.scss'
 
 export const FileSystemPane = () => {
+    const fileTypeMap = (type: string): string => {
+        const map: {[key: string]: string} = {
+            "file": "=FILE======>",
+            "directory": "=DIRECTORY=>"
+        };
+
+        return map[type];
+    };
+
+    const files = [
+        {
+            title: "TempFile1",
+            type: "directory"
+        },
+        {
+            title: "TempFile2",
+            type: "file"
+        },
+        {
+            title: "TempFile3",
+            type: "file"
+        },
+        {
+            title: "TempFile4",
+            type: "file"
+        },
+        {
+            title: "TempFile5",
+            type: "file"
+        },
+    ];
+
     return (
         <div className={styles.filesystem}>
             <div className={styles.fileselector_title}>
-                title
+                {files.map((file) => {
+                    return (
+                        <div>
+                            {file.title}
+                        </div>
+                    );
+                })}
             </div>
             <div className={styles.fileselector_type}>
-                type
+                {files.map((file) => {
+                    return (
+                        <div>
+                            {fileTypeMap(file.type)}
+                        </div>
+                    );
+                })}
             </div>
         </div>
     )
