7544a80595f749500aa63bc83b5b3f5534368fac
unknown <brz0323@gmail.com>
2025-07-20 23:13:15 -0500

    Implement path switching

--- src/TerminalPage/components/Path.tsx
+++ src/TerminalPage/components/Path.tsx
@@ -1,10 +1,14 @@
 import styles from '../TerminalPage.module.scss';
 import {Time} from "./Time.tsx";
+import {useContext} from "react";
+import {AppContext} from "../AppContext.tsx";
 export const Path = () => {
+    const { filePath } = useContext(AppContext);
+
     return (
         <div className={styles.path}>
             <div>
-                ~/temp/path
+                {filePath}
             </div>
             <div>
                 <Time />
