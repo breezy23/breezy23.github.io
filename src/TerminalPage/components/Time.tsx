8a37cd303aa311ee96e630b7e0bdebc624a631bd
unknown <brz0323@gmail.com>
2025-07-20 22:31:14 -0500

    Add time

---
+++ src/TerminalPage/components/Time.tsx
@@ -0,0 +1,40 @@
+import {useEffect, useState} from "react";
+import styles from '../TerminalPage.module.scss'
+
+export const Time = () => {
+    const [currentTime, setCurrentTime] = useState(new Date());
+    const [currentStyle, changeStyle] = useState("tick")
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 500);
+
+        return () => clearInterval(timer);
+    }, []);
+
+    const hours = currentTime.getHours();
+    const hours12 = hours % 12 === 0 ? 12 : hours % 12;
+    const minutes = currentTime.getMinutes();
+    const milliseconds = currentTime.getMilliseconds();
+
+    const hoursFormatted = hours12 < 10 ? `0${hours12}` : hours12;
+    const minutesFormatted = minutes < 10 ? `0${minutes}` : minutes;
+    const shouldTick = milliseconds < 500;
+    return (
+        <div>
+            <span>
+                {hoursFormatted}
+            </span>
+            <span className={shouldTick ? styles.tick : styles.tock}>
+                :
+            </span>
+            <span>
+                {minutesFormatted}
+            </span>
+            <span>
+                {hours >= 12 ? 'PM' : 'AM'}
+            </span>
+        </div>
+    );
+}
