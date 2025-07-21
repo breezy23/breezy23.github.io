1ffa48ca8e689826e3f21505a38b64c3a63fbeda
unknown <brz0323@gmail.com>
2025-07-20 19:49:52 -0500

    Handle keyboard input

--- src/TerminalPage/components/FileSystem.tsx
+++ src/TerminalPage/components/FileSystem.tsx
@@ -1,8 +1,28 @@
 import styles from '../TerminalPage.module.scss'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export const FileSystemPane = () => {
     const [selectedIndex, setSelectedIndex] = useState(0);
+
+    const setSelection = () => {
+        console.log(selectedIndex)
+    }
+    const changeSelection = (key: string) => {
+        switch (key) {
+            case 'ArrowUp':
+                setSelectedIndex(prev => (prev > 0 ? prev - 1 : 0));
+
+                break;
+            case 'ArrowDown':
+                setSelectedIndex(prev => (prev < files.length - 1 ? prev + 1 : prev));
+
+                break;
+            case 'Enter':
+                setSelection();
+
+                break;
+        }
+    }
 
     const fileTypeMap = (type: string): string => {
         const map: {[key: string]: string} = {
@@ -36,24 +56,40 @@ export const FileSystemPane = () => {
         },
     ];
 
+    useEffect(() => {
+        const handleKeyPress = (event: KeyboardEvent) => {
+            changeSelection(event.code)
+        };
+
+        window.addEventListener('keydown', handleKeyPress);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress)
+        }
+    }, [selectedIndex])
+
     return (
         <div className={styles.filesystem}>
             <div className={styles.fileselector_title}>
-                {files.map((file) => {
-                    const isSelected = true;
+                {files.map((file, index) => {
+                    const isSelected = index === selectedIndex;
                     return (
-                        <div className={`${styles.text} ${isSelected ? styles.selected : ''}`}>
+                        <div
+                            className={`${styles.text} ${isSelected ? styles.selected : ''}`}
+                            key={file.title}
+                        >
                             {file.title}
                         </div>
                     );
                 })}
             </div>
             <div className={`${styles.fileselector_type}`}>
-                {files.map((file) => {
-                    const isSelected = true;
+                {files.map((file, index) => {
+                    const isSelected = index === selectedIndex;
 
                     return (
-                        <div className={`${styles.text} ${isSelected ? styles.selected : ''}`}>
+                        <div
+                            className={`${styles.text} ${isSelected ? styles.selected : ''}`} key={`${file.title}-type`}>
                             {fileTypeMap(file.type) }
                         </div>
                     );
