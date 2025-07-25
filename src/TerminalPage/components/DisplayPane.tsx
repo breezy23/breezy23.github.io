cc1e133f0b7f0ac154d9cf2bb45dce29bd057a8b
Brandon Zaragoza <breezy23@users.noreply.github.com>
2025-07-25 15:51:55 -0500

    Fix some typescript errors

--- src/TerminalPage/components/DisplayPane.tsx
+++ src/TerminalPage/components/DisplayPane.tsx
@@ -1,10 +1,10 @@
 import styles from '../styles/display.module.scss';
 import {Resume} from "./display/Resume.tsx";
 import {ReadMe} from "./display/Readme.tsx";
-import {JSX, useContext} from "react";
+import {type JSX, useContext} from "react";
 import {AppContext} from "../AppContext.tsx";
 
-export const DisplayPane = (documentTitle: string) => {
+export const DisplayPane = () => {
     const { currentDocument } = useContext(AppContext)
     const componentToDisplay = (title: string) => {
         const displayMap: { "README.txt": JSX.Element; "Resume.md": JSX.Element } = {
